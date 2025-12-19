7aa9d99d5b579210e3fcac16e93b1ad052ad6b84
Abhay <abhaypvt2903@gmail.com>
2025-12-19 20:14:26 +0530

    Add CurrentThread method in FirstThreadDemo class to set and get name

--- FirstThreadDemo.java
+++ FirstThreadDemo.java
@@ -1,7 +1,7 @@
 class FirstThreadDemo extends Thread {
 
     public void run() {
-
+       Thread.currentThread().getName();
         for(int i=0; i<=10; i++){
             System.out.println(i);
         }
--- FirstThreadDemoMain.java
+++ FirstThreadDemoMain.java
@@ -3,7 +3,9 @@ public class FirstThreadDemoMain {
         System.out.println("good");
         System.out.println("Hello");
         FirstThreadDemo ab = new FirstThreadDemo();
+        ab.setName("Abhayyyy");
         ab.start();
+        System.out.println(ab.getName());
         System.out.println("GoodBYe");
         System.out.println("Abhay");
         
